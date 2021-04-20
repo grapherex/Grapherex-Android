b74b0b382e9bb18df1e1d3da5841ff023f979646
IvanDanilov <esseckers@gmail.com>
2021-04-20 20:59:14 +0300

    add new icon, and rename signal to grapherex

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -f whisper.store
+for filename in grapherex_dev.cer; do
+  keytool -importcert -v -trustcacerts -file "$filename" -alias "$filename" -keystore "whisper.store" -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath "/Users/esseckers/Downloads/bcprov-jdk16-1.45.jar" -storetype BKS -storepass whisper -noprompt -v
+done
